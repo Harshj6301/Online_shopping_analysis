df8c3796e4c9699b3e9c8c8d355c471db929056d
Harshvardhan Jadhav <jadhavharshvardhan6301@gmail.com>
2023-07-19 19:40:30 +0530

    Create app.py
    Testing streamlit dashboard

---
+++ app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title("Ecommerce Data Analysis")
+
+DATA_PATH = ("assets/Retail.OrderHistory.Clean.csv")
+
+def load_data(nrows):
+  data = pd.read_csv(DATA_PATH, nrows=nrows)
+  data.drop(columns=['Purchase Order Number','Gift Message','Gift Sender Name','Gift Recipient Contact Details','Unnamed: 19'],inplace=True)
+  data['Ship Date'] = pd.to_datetime(data['Ship Date'])
+  data['Order Date'] = pd.to_datetime(data['Order Date'])
+  return data
+
+def feature_cleaner(feature_list,dataset):
+    """The function replaces ','(comma) and '''(quote) in the entries and later converts all records into float64 """
+    for feature in feature_list:
+        dataset[feature] = dataset[feature].str.replace(',','')
+        dataset[feature] = dataset[feature].str.replace("'",'')
+   # print('Data cleaned..\nConverting to numeric type')
+    dataset[features_to_float] = dataset[features_to_float].astype('float64')
+    return dataset[features_to_float]
+
+data = feature_cleaner(feature_list=features_to_float,dataset=data)
+
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
+
+st.subheader('Pie chart')
+np.pie(data_frame=data[:10],values='Price',hover_data=['Product Name'],names='shorted_names',hole=0.5)
