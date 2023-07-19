1820472b7b0e762a4c97bef757c7399560fbb890
Harshvardhan Jadhav <jadhavharshvardhan6301@gmail.com>
2023-07-19 20:37:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,10 @@ fig1 = px.pie(data_frame=data[:10],values='Unit Price',hole=0.5, hover_data=['Pr
 st.plotly_chart(fig1, theme='streamlit', use_container_width=True)
 
 st.subheader('Pie chart for Last 20 prodcuts')
-price_slider = st.slider('Unit Price',0,max('Unit Price'))
-fig2 = px.pie(data_frame=price_slider,values='Unit Price',hole=0.5,hover_data=['Product Name'])
+fig2 = px.pie(data_frame=data[:19],values='Unit Price',hole=0.5,hover_data=['Product Name'])
 st.plotly_chart(fig2, theme='streamlit', use_container_width = True)
 
+st.subheader('Orders over time')
+fig3 = px.line(x='Order Date', y='Total Owed',data_frame=orders_df,
+       title='Orders through the time',hover_data=['Product Name'],labels={'Total Owed':'Price'},color='Quantity',line_dash_sequence=['dot'],markers=True)
+st.plotly_chart(fig3)
